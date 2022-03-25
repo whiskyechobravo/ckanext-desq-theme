9b2ce47b71bb9088d9fba9b0c089c38054240d1e
David Lesieur <david@davidlesieur.com>
2022-03-25 13:08:41 -0400

    Remove unwanted line break.

--- ckanext/desq_theme/plugin.py
+++ ckanext/desq_theme/plugin.py
@@ -10,5 +10,4 @@ class DesqThemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'desq_theme')
+        toolkit.add_resource('fanstatic', 'desq_theme')
